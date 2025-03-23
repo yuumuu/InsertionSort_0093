d732787e692aec3a473be77511f2bb7e79554590
yuumuu <haidar.alhazen93@gmail.com>
2025-03-23 22:54:11 +0700

    Membuat kode algoritma InsertionSort

--- main.cpp
+++ main.cpp
@@ -29,6 +29,27 @@ void Input()
     }
 }
 
+void InsertionSort()
+{
+    int temp;
+    int j, i;
+
+    for (i = 1; i <= n - 1; i++)
+    {
+        temp = arr[i];
+
+        j = i - 1;
+
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+
+        arr[j + 1] = temp;
+    }
+}
+
 int main()
 {
     
