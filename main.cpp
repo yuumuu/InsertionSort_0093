5f4e989b3cf42cd1b84b1ac5ae4ad9c3fefc31c8
yuumuu <haidar.alhazen93@gmail.com>
2025-03-23 22:53:36 +0700

    Proses memasukkan data inputan ke array

--- main.cpp
+++ main.cpp
@@ -17,6 +17,16 @@ void Input()
             cout << "\nArray yang anda masukkan masimal 20 elemen.\n";
         }
     }
+    
+    cout << endl;
+    cout << "=========================" << endl;
+    cout << "Masukkan elemen Array : " << endl;
+    cout << "=========================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << " : ";
+        cin >> arr[i];
+    }
 }
 
 int main()
