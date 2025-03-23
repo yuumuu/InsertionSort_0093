2f825b9dea4d80bb48116160d6e229dcd2823294
yuumuu <haidar.alhazen93@gmail.com>
2025-03-23 22:52:57 +0700

    Membuat kode Insert data Array

--- main.cpp
+++ main.cpp
@@ -5,7 +5,21 @@ using namespace std;
 int arr[20];
 int n;
 
+void Input()
+{
+    while (true) {
+        cout << "Masukkan Jumlah Data pada Arrah : ";
+        cin >> n;
+
+        if (n <= 20) {
+            break;
+        } else {
+            cout << "\nArray yang anda masukkan masimal 20 elemen.\n";
+        }
+    }
+}
+
 int main()
 {
-
+    
 }
